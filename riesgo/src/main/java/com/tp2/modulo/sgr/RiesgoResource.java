3c25462bc74ca4678d870e99923358769de55ab6
Franco <Franco@PC-FRANCO-10>
2018-11-03 09:12:58 -0500

    metodo /obtenerNumeroRiegosPorNivel metodo "/getTipoRiesgo")

--- riesgo/src/main/java/com/tp2/modulo/sgr/RiesgoResource.java
+++ riesgo/src/main/java/com/tp2/modulo/sgr/RiesgoResource.java
@@ -1,6 +1,9 @@
 package com.tp2.modulo.sgr;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -10,11 +13,13 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
 import com.tp2.modulo.sgr.model.RespuestaResponse;
 import com.tp2.modulo.sgr.model.Riesgo;
+import com.tp2.modulo.sgr.model.TipoRiesgo;
 import com.tp2.modulo.sgr.service.RiesgoService;
 
 @Path("/")
@@ -68,4 +73,35 @@ public class RiesgoResource {
 		
 		return json;
 	}
+	
+	  @GET
+	  @Path("/obtenerNumeroRiegosPorNivel")
+	  @Produces({"application/json"})
+	  public String obtenerNumeroRiesgosPorNivel(@QueryParam("anio") int anio, @QueryParam("tipoRiesgo") int tipoRiesgo, @QueryParam("mes") int mes)
+	  {
+	    Map<String, Integer> map = riesgoService.obtenerNumeroRiesgosPorNivel(Integer.valueOf(anio), Integer.valueOf(mes), Integer.valueOf(tipoRiesgo));
+	    
+	    String json = gson.toJson(map);
+	    
+	    return json;
+	  }
+	  
+	  @GET
+	  @Path("/getTipoRiesgo")
+	  @Consumes({"application/json"})
+	  @Produces({"application/json"})
+	  public String getTipoRiesgo()
+	  {
+	    HashMap<String, Object> map = new LinkedHashMap();
+	    
+	    ArrayList<TipoRiesgo> listaTipoRiesgo = riesgoService.getListaTipoRiesgo();
+	    
+	    System.out.print(listaTipoRiesgo.toString());
+	    
+	    map.put("listaRevisiones", listaTipoRiesgo);
+	    String json = gson.toJson(map);
+	    return json;
+	  }
+	
+	
 }
